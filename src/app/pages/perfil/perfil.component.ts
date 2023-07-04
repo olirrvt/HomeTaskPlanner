06e513df6b9ff6af9704c9cfda9daf9177304c04
Taylor Oliveira <taylorso2004@hotmail.com>
2023-07-04 19:54:28 -0300

    Adicionando área de perfil do usuário

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Morador } from 'src/app/models/Morador';
+import { AuthService } from 'src/app/services/Auth/auth.service';
 
 @Component({
   selector: 'app-perfil',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent {
+  morador!: Morador;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.getMoradorLogado();
+  }
+
+  getMoradorLogado(): void {
+    this.authService.getMoradorLogado().subscribe((res) => {
+      this.morador = res;
+      console.log(this.morador);
+    });
+  }
+
+  editarPerfil():void {
+    console.log("Perfil");
+  }
+
+  apagarConta(): void {
+    console.log("Apagar");
+  }
 
 }
+
