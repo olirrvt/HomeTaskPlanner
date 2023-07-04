069edc859b2615260a92a95086e8c9bb0e9d1ebc
Taylor Oliveira <taylorso2004@hotmail.com>
2023-07-04 14:36:29 -0300

    Adicionando botão de apagar reservas

--- src/app/pages/areas-comuns/areas-comuns.component.ts
+++ src/app/pages/areas-comuns/areas-comuns.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { interval } from 'rxjs';
 import { Reserva } from 'src/app/models/Reserva';
-import { MoradorService } from 'src/app/services/Morador/morador.service';
+import { AuthService } from 'src/app/services/Auth/auth.service';
 import { ReservaService } from 'src/app/services/Reserva/reserva.service';
 
 @Component({
@@ -11,22 +12,46 @@ import { ReservaService } from 'src/app/services/Reserva/reserva.service';
 })
 export class AreasComunsComponent {
   formularioReserva!: FormGroup;
+
   reservasDisponiveis: Reserva[] = [];
+  reservaMorador: Reserva[] = [];
+  moradorNome: string = ""; // Nome do morador logado
   
   constructor (
     private reservaService: ReservaService,
-    private moradorService: MoradorService,
+    private authService: AuthService,
     private formBuilder: FormBuilder
     ) { }
 
     ngOnInit(): void {
       this.inicializarFormularioReserva();
       this.buscarReservasDisponiveis();
+      this.buscarReservasDoMorador();
+
+      interval(60000).subscribe(() => {
+        this.verificarExpiracaoReservas();
+      });
     };
 
     private buscarReservasDisponiveis(): void {
       this.reservaService.getAllReservas().subscribe((reservas) => {
         this.reservasDisponiveis = reservas;
+      });
+    }
+
+    apagarReserva(reservaId: number): void {
+      this.reservaService.deleteReserva(reservaId).subscribe(() => {
+        // Remover a reserva apagada da lista de reservas do morador logado
+        this.reservaMorador = this.reservaMorador.filter(reserva => reserva.id !== reservaId);
+        // Remover a reserva apagada da lista de reservas disponíveis (caso ela esteja nessa lista)
+        this.reservasDisponiveis = this.reservasDisponiveis.filter(reserva => reserva.id !== reservaId);
+      });
+    }
+
+    private buscarReservasDoMorador(): void {
+      this.authService.getMoradorLogado().subscribe(res => {
+        this.moradorNome = res.nome;
+        this.reservaMorador = res.reservas;
       });
     }
 
@@ -73,7 +98,7 @@ export class AreasComunsComponent {
 
     EnviarFormularioReserva(): void {
       const reserva: Reserva = this.formularioReserva.value;
-
+    
       if (reserva.espacoComum) {
         reserva.espacoComum = reserva.espacoComum.toLowerCase();
       }
@@ -82,8 +107,37 @@ export class AreasComunsComponent {
         this.reservado = true;
         this.reservaExistente = false;
         this.reservaInvalida = false;
+    
+        // Adicionar a reserva criada à lista de reservas do morador logado
+        this.reservaMorador.push(res);
+        // Adicionar a reserva criada à lista de reservas disponíveis
+        this.reservasDisponiveis.push(res);
       });
+    }    
 
+    private verificarExpiracaoReservas(): void {
+      const agora = new Date();
+      
+      const reservasExpiradas = this.reservasDisponiveis.filter(reserva => {
+        const dataReserva = new Date(reserva.dataHoraReserva);
+        return agora >= dataReserva;
+      });
+    
+      if (reservasExpiradas.length > 0) {
+        reservasExpiradas.forEach(reservaExpirada => {
+          
+          this.reservaService.deleteReserva(reservaExpirada.id).subscribe(() => {
+            this.reservasDisponiveis = this.reservasDisponiveis.filter(reserva => reserva.id !== reservaExpirada.id);
+          });
+    
+          // Simulando exclusão da reserva sem servidor real
+          this.reservasDisponiveis = this.reservasDisponiveis.filter(reserva => reserva.id !== reservaExpirada.id);
+    
+          // Se a reserva expirada estiver na lista do morador logado, também remover
+          this.reservaMorador = this.reservaMorador.filter(reserva => reserva.id !== reservaExpirada.id);
+        });
+      }
     }
+    
 
 }
