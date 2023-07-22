8eec33d0801ed8949fc18af2e1d28ce6e1ad1316
Taylor Oliveira <taylorso2004@hotmail.com>
2023-07-22 18:29:09 -0300

    Modificações no Header Logado

--- src/app/pages/cadastrar/cadastrar.component.ts
+++ src/app/pages/cadastrar/cadastrar.component.ts
@@ -21,6 +21,7 @@ export class CadastrarComponent {
   msgSenha: string = "";
 
   // Regex para a validação dos campos ainda não implementado
+
   nomeRegex = /^[a-zA-Z ]{2,30}$/;
   apartamentoRegex = /^\d{2}[A-Za-z]?\d{2}$/;
   emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -38,32 +39,21 @@ export class CadastrarComponent {
 
   private inicializarFormulario(): void {
     this.formulario = this.formBuilder.group({
-      nome: ['', [Validators.required, Validators.pattern(this.nomeRegex)]],
-      apartamento: ['', [Validators.required, Validators.pattern(this.apartamentoRegex)]],
-      email: ['', [Validators.required, Validators.pattern(this.emailRegex)]],
-      senha: ['', [Validators.required, Validators.pattern(this.senhaRegex)]],
+      nome: ['', [Validators.required ] ],
+      apartamento: ['', [Validators.required] ],
+      email: ['', [Validators.required] ],
+      senha: ['', [Validators.required] ],
       isAdministrador: [false]
     });
   }
   
   EnviarFormulario(): void {
-    console.log(this.formulario.valid);
-
-    if (this.formulario.valid) {
       this.inputInvalid = false;
       const morador: Morador = this.formulario.value;
   
       this.moradorService.postMorador(morador).subscribe(resultado => {
         this.modalService.exibirModal();
       });
-    } else {
-      console.log();
-      this.inputInvalid = true;
-      this.msgNome = this.formulario.controls['nome'].errors?.['required'] ? "O nome deve ter letras maiúsculas e minúsculas, espaços e ter um comprimento de no mínimo 2 caracteres e máximo de 30 caracteres." : "";
-      this.msgApt = this.formulario.controls['apartamento'].errors?.['required'] ? "O número do apartamento deve seguir o formato '08102', onde os dois primeiros dígitos representam o bloco e os dois últimos dígitos representam o número do apartamento." : "";
-      this.msgEmail = this.formulario.controls['email'].errors?.['required'] ? "O formato do email deve seguir um padrão básico" : "";
-      this.msgSenha = this.formulario.controls['senha'].errors?.['required'] ? "Requer uma senha com pelo menos 8 caracteres, incluindo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial." : "";
-    }
   }
 
 }
